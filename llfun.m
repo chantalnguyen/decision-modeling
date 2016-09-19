b5d1ff7638d53d5436d88bf92854d03f7b877f57
Chantal Nguyen <chantalxnguyen@gmail.com>
2016-09-19 13:51:17 -0700

    got rid of real() in llfun, mle_ind50 gives proper results now

--- llfun.m
+++ llfun.m
@@ -19,10 +19,14 @@ if space == 50
 %             P_hit_range(i).^theta(2) + theta(3));
 %     end
 else
+%     for i = 1:length(P_hit_range)
+%         ll = ll + (H(i) - J(i))*log(1 - theta(1)*sign(P_hit_range(i)).*abs(P_hit_range(i)^(theta(2)/space)))...
+%             + J(i)*log(theta(1)*sign(P_hit_range(i)).*abs(P_hit_range(i)^(theta(2)/space)));
+%     end
     for i = 1:length(P_hit_range)
-        ll = ll + (H(i) - J(i))*log(1 - theta(1)*sign(P_hit_range(i)).*abs(P_hit_range(i)^(theta(2)/space)))...
-            + J(i)*log(theta(1)*sign(P_hit_range(i)).*abs(P_hit_range(i)^(theta(2)/space)));
+        ll = ll + (H(i) - J(i))*log(1 - theta(1)*sign(P_hit_range(i)).*abs(P_hit_range(i)^(space/theta(2))))...
+            + J(i)*log(theta(1)*sign(P_hit_range(i)).*abs(P_hit_range(i)^(space/theta(2))));
     end
 end
-ll = -1*ll;
+ll = (-1*ll);
 end
